32be0f63fe1fddcc1f61c0fae7c0948926fa575f
MercuryHgO <steb.mixa@gmail.com>
2024-09-26 00:16:29 +0000

    Added GUI

--- shell.nix
+++ shell.nix
@@ -1,13 +1,62 @@
-{ pkgs ? import <nixpkgs> {}}:
-with pkgs; mkShell {
-  buildInputs = [
-    rustc
-    cargo
-    rust-analyzer
-    gdb
-  ];
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { 
+    overlays = [ rust_overlay];
+  };
+  rustVersion = "latest";
+  # rustVersion = "1.62.0";
+  rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+    extensions = [
+      "rust-src" # for rust-analyzer
+      "rust-analyzer"
+    ];
+    targets = [
+      "x86_64-pc-windows-gnu"
+    ];
+  };
+in
+(pkgs.buildFHSEnv {
+  name = "money_counter_dev";
   
-  shellHook = ''
-    echo Monuey Counter Project
+  targetPkgs = pkgs: [
+    rust
+  ] ++ (with pkgs; [
+    zlib
+    gcc
+    pkgsCross.mingwW64.stdenv.cc
+    wayland
+    xorg.libX11
+    libxkbcommon
+    pkg-config
+  ]) ++ (with pkgs.xorg; [
+    libxcb
+    libXcursor
+    libXrandr
+    libXi
+  ]);
+
+  profile = with pkgs;
+  ''
+    export RUST_BACKTRACE=1
+    export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUSTFLAGS="-L native=${pkgsCross.mingwW64.windows.pthreads}/lib"
+    export LD_LIBRARY_PATH=/run/opengl-driver/lib/:${lib.makeLibraryPath ([libGL libGLU])}
+    # export PKG_CONFIG_PATH="${gtk3.dev}/lib/pkgconfig:${glib.dev}/lib/pkgconfig:${cairo.dev}/lib/pkgconfig:${pango.dev}/lib/pkgconfig:${harfbuzz.dev}/lib/pkgconfig:${gdk-pixbuf.dev}/lib/pkgconfig:${atk.dev}/lib/pkgconfig"
   '';
-}
+  
+
+  runScript = "${pkgs.writeShellScriptBin "dev_env" ''
+    tmux new-session -d -t money_counter_dev
+
+    tmux split-window -h -t money_counter_dev
+    tmux resize-pane -t money_counter_dev:0.1 -x 20%
+
+    tmux send-keys -t money_counter_dev:0 'bash' C-m
+
+    tmux send-keys -t money_counter_dev:0.0 'hx' C-m
+
+    tmux attach-session -t money_counter_dev
+
+    while tmux has-session -t money_counter_dev; do sleep 1; done
+    exit
+  ''}/bin/dev_env";
+}).env
--- src/main.rs
+++ src/main.rs
@@ -1,161 +1,113 @@
-use std::env::args;
+use eframe::{egui::{CentralPanel, TextBuffer}, Result};
+use money::{Money, Unit};
+use rust_decimal::{prelude::FromPrimitive, Decimal};
 
-use rust_decimal::Decimal;
+mod money;
 
-#[derive(Debug)]
-struct Unit(u128);
+struct MyApp {
+    budget: Money,
+    price: Money,
+    count: Unit
+}
 
-impl Unit {
-    fn pronounce(&self) -> String {
-        let pronounce: &'static str = match self.0 % 10 {
-            1 if self.0 % 100 != 11 => "единицу",
-            2..=4 if !(self.0 % 100 >= 12 && self.0 % 100 <= 14) => "единицы",
-            _ => "единиц"
-        };
-        format!("{} {}", self.0, pronounce)
+impl Default for MyApp {
+    fn default() -> Self {
+        MyApp { budget: Money(Decimal::from(0)), price: Money(Decimal::from(0)), count: Unit(0) }
     }
 }
 
-impl std::fmt::Display for Unit {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f,"{}",self.pronounce())
+impl MyApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
     }
 }
 
-impl From<&Unit> for Decimal {
-    fn from(value: &Unit) -> Self {
-        Decimal::from_parts(
-            (value.0 & 0xFFFFFFFF) as u32,
-            ((value.0 >> 32) & 0xFFFFFFFF) as u32,
-            ((value.0 >> 64) & 0xFFFFFFFF) as u32,
-            false,
-        0)
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
+        CentralPanel::default()
+            .show(ctx, |ui| {
+                let summ = self.price * Money(self.count.0.into());
+                let leftover = self.budget - self.price * self.count;
+
+                ui.horizontal(
+                    |ui| {
+                        ui.label("Бюджет: ");
+                        ui.text_edit_singleline(&mut self.budget);
+                    });
+                ui.label(format!("{}",self.budget));
+                ui.horizontal(
+                    |ui| {
+                        ui.label("Цена: ");
+                        ui.text_edit_singleline(&mut self.price);
+                    });
+                ui.label(format!("{}",self.price));
+                ui.horizontal(
+                    |ui| {
+                        ui.label("Количество: ");
+                        ui.text_edit_singleline(&mut self.count);
+                    }
+                );
+                ui.label(format!("{}",self.count));
+                ui.label(format!("Итоговая сумма: {} за {}.\n{} {}",
+                    summ,
+                    self.count,
+                    if leftover.0.is_sign_positive() {
+                        "В остатке:"
+                    } else {
+                        "Не хватает: "
+                    },
+                    leftover.abs()
+                ));
+
+            });
     }
 }
 
-#[derive(Debug)]
-struct Money(Decimal);
+fn main() {
+    let options = eframe::NativeOptions::default();
 
-impl Money {
-    fn pronounce(&self) -> core::result::Result<String, std::fmt::Error> {
-        let integer_amount: u128 = self.0.trunc()
-            .try_into()
-            .map_err(|_| std::fmt::Error)?;
-
-        let rubles_pronounce: &'static str = match integer_amount % 10 {
-            1 if integer_amount % 100 != 11 => "рубль",
-            2..=4 if !(integer_amount % 100 >= 12 && integer_amount % 100 <= 14) => "рубля",
-            _ => "рублей"
-        };
-
-        if self.0.is_integer() {
-            Ok(format!("{} {}",integer_amount, rubles_pronounce))
-        } else {
-            let unwrapped_fraction = self.0.fract().unpack();
-            let fraction_amount: u8 = Decimal::from_parts(
-                unwrapped_fraction.lo, 0, 0, false, 0)
-                .try_into()
-                .map_err(|_| std::fmt::Error)?;
-
-            let kopek_pronounce: &'static str = match fraction_amount % 10 {
-                1 if fraction_amount % 100 != 11 => "копейка",
-                2..=4 if !(fraction_amount % 100 >= 12 && fraction_amount % 100 <= 14) => "копейки",
-                _ => "копеек"
-            };
-
-
-            Ok(format!("{} {} {} {}",integer_amount, rubles_pronounce, fraction_amount, kopek_pronounce))
-        }
-    }
+    eframe::run_native(
+        "Money counter",
+        options,
+        Box::new(|ctx| {
+            Ok( Box::new(MyApp::new(ctx) ))
+        })           
+    ).unwrap();
 }
 
-impl TryFrom<String> for Money {
-    type Error = rust_decimal::Error;
 
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        let number = Decimal::from_str_exact(&value)?;
-        Ok(Money(number))
-    }
-}
+// fn main() -> anyhow::Result<()> {
+//     let budget: Money = match args().nth(1) {
+//         Some(val) => {
+//             let dec = Decimal::from_str_exact(&val)?;
+//             dec.try_into()?
+//         },
+//         None => {
+//             Err(money::Error::BudgetNotPresented)?
+//         },
+//     };
 
-impl TryFrom<Decimal> for Money {
-    type Error = Error;
+//     const PRICE: Money = Money(Decimal::from_parts(8999,0 ,0 ,false , 2));
 
-    fn try_from(value: Decimal) -> Result<Self,Self::Error> {
-        let unpacked = value.unpack();
+//     let count = budget.into() / PRICE.into();
+//     let units = Unit(count.trunc().try_into()?);
 
-        if unpacked.negative == true { return Err(Error::NegativeValue); };
-        if unpacked.scale > 2 { return Err(Error::KopekDigitsTooBig); };
-        
-        Ok(Money(value))
-    }
-}
+//     let summ: Money = Money(
+//         PRICE.into() * Decimal::from(&units)
+//     );
 
-impl std::fmt::Display for Money {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.pronounce()?)
-    }
-}
+//     let leftover = Money(
+//         budget.0 - summ.0
+//     );
 
-#[derive(Debug)]
-enum Error {
-    NegativeValue,
-    BudgetNotPresented,
-    KopekDigitsTooBig,
-    RustDecimalError(rust_decimal::Error)
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::NegativeValue => write!(f,"Число не может быть отрицательным"),
-            Error::BudgetNotPresented => write!(f,"Бюджет не предоставлен"),
-            Error::RustDecimalError(c) => std::fmt::Display::fmt(c,f),
-            Error::KopekDigitsTooBig => write!(f,"Неверно указано количество копеек, правильное значение: [рубли].(1-99)"),
-        }
-    }
-}
-
-impl From<rust_decimal::Error> for Error {
-    fn from(value: rust_decimal::Error) -> Self {
-        Error::RustDecimalError(value)
-    }
-}
-
-impl std::error::Error for Error { }
-
-fn main() -> anyhow::Result<()> {
-    let budget: Money = match args().nth(1) {
-        Some(val) => {
-            let dec = Decimal::from_str_exact(&val)?;
-            dec.try_into()?
-        },
-        None => {
-            Err(Error::BudgetNotPresented)?
-        },
-    };
-
-    const PRICE: Money = Money(Decimal::from_parts(8999,0 ,0 ,false , 2));
-
-    let count = budget.0 / PRICE.0;
-    let units = Unit(count.trunc().try_into()?);
-
-    let summ: Money = Money(
-        PRICE.0 * Decimal::from(&units)
-    );
-
-    let leftover = Money(
-        budget.0 - summ.0
-    );
-
-    println!("Размер бюджета: {}",budget);
-    println!("Цена за единицу товара: {}",PRICE);
+//     println!("Размер бюджета: {}",budget);
+//     println!("Цена за единицу товара: {}",PRICE);
 
 
-    print!("За {} можно приобрести {} товара ",budget,units);
-    println!("на сумму {}, имея в остатке {}",summ,leftover);
+//     print!("За {} можно приобрести {} товара ",budget,units);
+//     println!("на сумму {}, имея в остатке {}",summ,leftover);
 
 
-    Ok(())
+//     Ok(())
     
-}
+// }
